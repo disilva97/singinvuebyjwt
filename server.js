6a615917f186f623baabd8703e7dda52da411868
aravind sana <sanaaravind97@gmail.com>
2021-07-22 13:29:28 +0530

    firt

---
+++ server.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/get", (req, res) => {
+  res.json({
+    message: "hello ma guy"
+  })
+})
+
+
+app.post("/login", (req, res) => {
+  const USERNAME = "uma victor";
+  const PASSWORD = "8888";
+
+  const { username, password } = req.body;
+
+  if (username === USERNAME && password === PASSWORD) {
+    const user = {
+      id: 1,
+      name: "uma victor",
+      username: "uma victor",
+    };
+    const token = jwt.sign(user, process.env.JWT_KEY);
+    res.json({
+      token,
+      user,
+    });
+  } else {
+    res.status(403);
+    res.json({
+      message: "wrong login information",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
